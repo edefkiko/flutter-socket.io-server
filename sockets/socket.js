c85207911c6daaffd197ce65ada45ba1939c548a
Federico Cruz <federico.odasystems@gmail.com>
2023-02-16 11:23:11 -0600

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,22 @@
 const { io } = require('../index')
+const Bands  = require('./../models/bands');
+const Band  = require('./../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Motorhead'));
+
+console.log(bands);
+
 //Mensajes de sockets
 io.on('connection', client => {
   console.log('Cliente conectado');
-  //client.on('event', data => { /* … */ });
+  
+  client.emit('active-bands', bands.getBands());
+  
   client.on('disconnect', () => { 
     console.log('Cliente desconectado');
   });
@@ -14,4 +28,27 @@ io.on('connection', client => {
       admin: 'Nuevo mensaje'
     });
   });
+
+  // client.on('emitir-mensaje', (payload)=>{
+  //   client.broadcast.emit('nuevo-mensaje', payload);
+  // });
+
+  client.on('vote-band', (payload)=>{
+    bands.voteBand(payload.id);
+
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('add-band', (payload)=>{
+    bands.addBand(new Band(payload.name));
+
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('delete-band', (payload)=>{    
+    bands.deleteBand(payload.id);
+
+    io.emit('active-bands', bands.getBands());
+  });
+
 });
